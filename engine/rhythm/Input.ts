6cd955639dde38643cb82f692592d0716b5bfb06
Ander <157061485+Brenninho123@users.noreply.github.com>
2026-01-15 11:01:05 -0300

    Atualizar o Input.ts

--- engine/rhythm/Input.ts
+++ engine/rhythm/Input.ts
@@ -19,3 +19,15 @@ export class Input {
 static isPressed(key: string) {
   return !!Input.keys[key];
 }
+
+static initTouch() {
+  window.addEventListener("touchstart", e => {
+    const x = e.touches[0].clientX;
+    const lane = Math.floor(x / (window.innerWidth / 4));
+    Input.keys["lane" + lane] = true;
+  });
+
+  window.addEventListener("touchend", () => {
+    Input.keys = {};
+  });
+}
