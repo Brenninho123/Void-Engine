b25cd99890785bcfd560414e5840b513828755e6
Ander <157061485+Brenninho123@users.noreply.github.com>
2026-01-15 10:20:50 -0300

    Add Conductor class for rhythm management

---
+++ engine/rhythm/Conductor.ts
@@ -0,0 +1,14 @@
+export class Conductor {
+  static bpm = 120;
+  static crochet = 0;
+  static songPosition = 0;
+
+  static init(bpm: number) {
+    this.bpm = bpm;
+    this.crochet = (60 / bpm) * 1000;
+  }
+
+  static update(deltaMs: number) {
+    this.songPosition += deltaMs;
+  }
+}
